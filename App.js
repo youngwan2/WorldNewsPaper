cdd9c740426737373d389489114eb4ebaf61a5f9
KYW <qodna25@gmail.com>
2022-10-27 19:50:31 +0900

    news paper code refactoring

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ let main = document.querySelector('.main')
 let userInput=document.getElementById('user-input')
 let inputBtn=document.getElementById('input-btn')
 const menuBtn=document.querySelectorAll('.container .menu button')
-menuBtn.forEach((menu)=>{menu.addEventListener("click",(e)=>getTopic(e))})
 
+
+menuBtn.forEach((menu)=>{menu.addEventListener("click",(e)=>getTopic(e))})
 menuBtn.forEach((btn)=>{
   btn.addEventListener('click',(e)=>{
    menuBtnLine(e)
@@ -12,32 +13,31 @@ menuBtn.forEach((btn)=>{
  })
 
 
+let url;
+
+const getDataStore=async()=>{
+  let response = await fetch(url)      
+  let data=await response.json();       
+  let getData=data.response.results
+
+  let temp=``
+      temp= getData.map((getData)=>{
+          return `<div class="main-content">
+          <div class="main-text">
+            <h4><a style="text-decoration:none; color:rgb(230, 49, 155)" href="${getData.webUrl}">${getData.webTitle}</a></h4><br>
+            <p>제목을 클릭하시면 해당 기사의 사이트로 이동하여 자세한 정보를 확인하실 수 있습니다.</p><br>
+            <div class="date">${getData.webPublicationDate}</div>
+          </div>
+          </div>` })
+      
+         main.innerHTML = temp.join(''); 
+}
 // 메인뉴스 리스트가 보이는 곳
 let news = [];
 const worldNews= async() =>{
-    let url =new URL('https://content.guardianapis.com/search?api-key=e14f2dc3-7231-47da-900e-d38cc4d26542')
-    console.log(url)  //url 정보를 가져온다.
-
-    let response = await fetch(url)      //url 로 부터 응답 정보를 받아온다.
-
-    let data=await response.json();        //응답 정보로 부터 body 정보를 받아온다.
-             
-    
-    let getResults=data.response.results
-  
-
-  
-    let temp=``
-        temp= getResults.map((getResults)=>{
-            return `<div class="main-content">
-            <div class="main-text">
-              <h4><a style="text-decoration:none; color:rgb(230, 49, 155)" href="${getResults.webUrl}">${getResults.webTitle}</a></h4><br>
-              <p>제목을 클릭하시면 해당 기사의 사이트로 이동하여 자세한 정보를 확인하실 수 있습니다.</p><br>
-              <div class="date">${getResults.webPublicationDate}</div>
-            </div>
-            </div>` })
-        
-           main.innerHTML = temp.join(''); 
+    url =new URL('https://content.guardianapis.com/search?api-key=e14f2dc3-7231-47da-900e-d38cc4d26542')
+ 
+    getDataStore();
        
        
 } 
@@ -48,53 +48,22 @@ const worldNews= async() =>{
 const getTopic=async(e)=>{
   console.log(e.target.textContent)
   let topic = e.target.textContent.toLowerCase()
-  let url= new URL(`https://content.guardianapis.com/search?q=${topic}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
+  url= new URL(`https://content.guardianapis.com/search?q=${topic}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
 
+  getDataStore();
 
-  let response = await fetch(url)
-  let data = await response.json()
-  let getData=data.response.results
-
-
-  let temp=''
-  getData.map((getData)=>{
-    return temp+=`<div class="main-content">
-      <div class="main-text">
-        <h4><a style="text-decoration:none; color:rgb(230, 49, 155)" href="${getData.webUrl}">${getData.webTitle}</a></h4><br>
-        <p>제목을 클릭하시면 해당 기사의 사이트로 이동하여 자세한 정보를 확인하실 수 있습니다.</p><br>
-        <div class="date">${getData.webPublicationDate}</div>
-      </div>
-      </div>` })
-  
-      main.innerHTML = temp
   }
  
+
 //검색창에 키워드 입력 시 해당하는 키워드의 뉴스 리스트가 뜨도록 코딩
-
-
 const searchNews=async()=>{
   let search=userInput.value
-  let url=new URL(`https://content.guardianapis.com/search?q=${search}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
-  let response= await fetch(url)
-  let data=await response.json()
+  url=new URL(`https://content.guardianapis.com/search?q=${search}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
+  
+ getDataStore();   
 
-  let getSearch=data.response.results
-  console.log(getSearch)
+ }
 
-  let searchHTML=''
-  getSearch.map((getSearch)=>{
-    return searchHTML+=`<div class="main-content">
-    <div class="main-text">
-      <h4><a style="text-decoration:none; color:rgb(230, 49, 155)" href="${getSearch.webUrl}">${getSearch.webTitle}</a></h4><br>
-      <p>제목을 클릭하시면 해당 기사의 사이트로 이동하여 자세한 정보를 확인하실 수 있습니다.</p><br>
-      <div class="date">${getSearch.webPublicationDate}</div>
-    </div>
-    </div>` })
-
-    main.innerHTML=searchHTML
-
- 
-}
 
 inputBtn.addEventListener('click',searchNews)
 
