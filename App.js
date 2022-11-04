5d43d4973e00d975f7a10a2bafc0b86c7a00a685
KYW <qodna25@gmail.com>
2022-11-04 23:17:43 +0900

    page nation add

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ let inputBtn=document.getElementById('input-btn');
 const menuBtn=document.querySelectorAll('.container .menu button');
 
 
+
+
+ 
 menuBtn.forEach((menu)=>{menu.addEventListener("click",(e)=>getTopic(e))});
 
 menuBtn.forEach((btn)=>{
@@ -13,18 +16,31 @@ menuBtn.forEach((btn)=>{
   });
  });
 
+ let pageNum =1        //현재페이지 임의값( 0이되면 오류가 뜨므로 1을 기본으로 설정)
+ let total = 120;        //전체페이지
 
-let url;
 
 
-const getDataStore=async()=>{
-  let response = await fetch(url)
-  let data= await response.json()    
-  let getData=data.response.results
 
+ let url;
+//============== 중복 데이터 저장소=================================
+const getDataStore = async()=>{
+ 
        try{
+        url.searchParams.set('page',pageNum)
+        console.log(url)
+        let response = await fetch(url)
+        let data = await response.json()    
+        let getData = data.response.results
+        let currentPage = data.response.currentPage
+
+
+
         if(response.status == 200){
-         
+          if(data.response.total==0){ //예외조건(오류가 아니지만 실행할)
+            throw new Error('검색된 자료가 존재하지 않습니다.')
+          };
+
           let temp=``
               temp= getData.map((getData)=>{
                   return `<div class="main-content">
@@ -36,61 +52,116 @@ const getDataStore=async()=>{
                   </div>` });
               
                  main.innerHTML = temp.join(''); 
-
         } else {
-           throw new Error(response.status)
+           throw new Error(response.status)  // status 가 200이 아니라면 그 외 메시지는 오류 처리한다.
         };
-
-        if(getData==''){
-          throw new Error('검색된 자료가 존재하지 않습니다.')
-        };
-
-       } catch(error){
+       } catch(error){    
            alert(error)
            main.innerHTML =`<div id='not-find'>${error}</div>`
        };
 
-       
+       pagenation() 
+       pageNum= 1
 };
 
 
-// 메인뉴스 리스트가 보이는 곳
+// ==============================메인뉴스 리스트가 보이는 곳==================================
 let news = [];
 const worldNews= async() =>{
-    url =new URL('https://content.guardianapis.com/search?api-key=e14f2dc3-7231-47da-900e-d38cc4d26542')
+   url =new URL(`https://content.guardianapis.com/search?api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
+
     getDataStore();
     
-};  worldNews();
+};  
 
 
-//카테고리 별 주제 클릭 시 해당하는 키워드의 뉴스 리스트를 호출
-const getTopic=async(e)=>{
+//================카테고리 별 주제 클릭 시 해당하는 키워드의 뉴스 리스트를 호출========================
+const getTopic = async(e) => {
   console.log(e.target.textContent)
   let topic = e.target.textContent.toLowerCase()
   url= new URL(`https://content.guardianapis.com/search?q=${topic}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
+
+
 
   getDataStore();
 
   };
  
 
-//검색창에 키워드 입력 시 해당하는 키워드의 뉴스 리스트가 뜨도록 코딩
-const searchNews=async()=>{
+//======================검색창에 키워드 입력 시 해당하는 키워드의 뉴스 리스트가 뜨도록 코딩=================
+const searchNews = async() => {
   let search=userInput.value
   url=new URL(`https://content.guardianapis.com/search?q=${search}&api-key=e14f2dc3-7231-47da-900e-d38cc4d26542`)
+
  
   getDataStore();  
   
  }; inputBtn.addEventListener('click',searchNews) 
 
 
+//====================================페이지네이션 함수-============================================
+let firstPageShift = document.querySelector('.first-page')
+let lastPageShift = document.querySelector('.last-page')
+
+
+const pagenation = (firstBeforePage) => {
+  
+  // 총 페이지
+  // 현재 페이지
+  
+  let pageGroup = Math.ceil(pageNum/5) //현재 페이지가 속한 그룹 = (현재 페이지 번호)/(한 화면에 보여줄 페이지 수)
+  console.log(pageGroup)
+  let lastPage = pageGroup * 5       //마지막 페이지 = 현재 페이지 그룹 * 한 화면에 보여줄 페이지 수
+  let firstPage = lastPage -(5-1)    //첫 번째 페이지 = 마지막페이지 -(한 화면에 보여줄 페이지수 - 1)
+
+
+  let pagenationHTML=`
+  <li class="page-item"><a onclick="page(${1})" class="first-page" href="#"><<</a></li>
+  <li class="page-item"><a onclick="page(${pageNum-1})" class="before" href="#"><</a></li>`
+  
+  for(let i=firstPage; i<=lastPage; i++){   //페이지네이션을 첫번째 페이지에서 마지막페이지 까지 그려준다.
+    pagenationHTML += `<li class="page-item"><a class= ${pageNum===i? "action" : "normal"} onClick="pageChange(${i})" class="page-link" href="#">${i}</a></li>`
+  }
+  pagenationHTML+=`
+  <li class="page-item"><a onClick="page(${pageNum+1})" class="after" href="#">></a></li>
+  <li class="page-item"><a onclick="page(${total})" class="last-page" href="#">>></a></li>`
+  document.querySelector('.pagenation').innerHTML = pagenationHTML
+
+};
+
+
+pagenation();
+
+//페이지 숫자 클릭 시 해당 번호의 페이지로 이동
+const pageChange=(num)=>{
+  pageNum = num
+  console.log(pageNum)
+
+  getDataStore()
+};
+
+// 이전-이후 페이지, 첫-마지막 페이지 이동
+const page=(num)=>{
+  pageNum = num
+  if(pageNum <1){
+    return alert("페이지가 존재하지 않습니다.")
+  }
+
+  if(pageNum >total){
+    return alert("마지막 페이지 입니다.")
+  }
+
+  getDataStore()
+};
 
 
 
 
 
 
-// 카테고리를 선택했을 때 해당하는 영역으로 라인이 이동하도록 코딩
+
+
+// -----------------------------------카테고리를 선택했을 때 해당하는 영역으로 라인이 이동하도록 코딩---------------------------------
 
 function menuBtnLine(e){
   line.style.display='block'
@@ -101,7 +172,7 @@ function menuBtnLine(e){
 };
 
 
-
+worldNews();
 
 
 
